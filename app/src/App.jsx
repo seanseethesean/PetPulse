a8ff726902eb4255d92a353a893c7972cce84397
Sean <takapvppro@gmail.com>
2025-05-24 15:02:00 +0800

    Backend folder + Login files
    Created the backend folder, changed the naming of the Login files

---
+++ app/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+
+export default App;
