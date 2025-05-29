bcc054ba9adbdffa3a6ecaa8b667ed52397dee44
seanseethesean <takapvppro@gmail.com>
2025-05-29 21:31:06 +0800

    Changed firebase account
    Changed the configuration for firebase account to petpulse gmail

--- app/src/firebase.jsx
+++ app/src/firebase.jsx
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-xvafDSofr7mzkgfy3ZMjBxRFeeu9sgs",
-    authDomain: "petpulse-bbbec.firebaseapp.com",
-    projectId: "petpulse-bbbec",
-    storageBucket: "petpulse-bbbec.firebasestorage.app",
-    messagingSenderId: "1058132065929",
-    appId: "1:1058132065929:web:02a0b86c46515e113c9058",
-    measurementId: "G-30B19F5X7E"
-};
+    apiKey: "AIzaSyCcq5MGnc2cVifz1wTi2aSngOn8Q0KyyBo",
+    authDomain: "petpulse-b1a02.firebaseapp.com",
+    projectId: "petpulse-b1a02",
+    storageBucket: "petpulse-b1a02.firebasestorage.app",
+    messagingSenderId: "868043579789",
+    appId: "1:868043579789:web:dab13c0e47ee6273091dc4",
+    measurementId: "G-WTQ39V2H67"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
