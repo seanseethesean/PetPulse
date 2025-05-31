f4eb7a2d81b0038ccbc6be9344773602d989a5bf
seanseethesean <takapvppro@gmail.com>
2025-06-01 00:29:50 +0800

    Update TaskChecklist.jsx

--- app/src/pages/TaskChecklist.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import '../assets/TaskChecklist.css';
-
-//help me in  importing the image
-//import check_icon from '../assets/images/check.png';
-import Navbar from '../components/Navbar';
-    
