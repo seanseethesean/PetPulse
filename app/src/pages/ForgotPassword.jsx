b83b2d3b6b8780b0c187498f35b2109e307c170c
ngjade <ng.jade05@gmail.com>
2025-05-31 13:14:28 +0800

    half way settling the backend

--- app/src/pages/ForgotPassword.jsx
+++ app/src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import { auth } from 'firebase.js';
+import { auth } from '../../firebase.js';
 import { useNavigate } from 'react-router-dom';
 import '../assets/ForgotPassword.css';
 
