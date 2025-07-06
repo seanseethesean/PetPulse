a43b41e01e97e1f9b0851f2563c99e81cb4a671a
seanseethesean <takapvppro@gmail.com>
2025-07-07 01:02:27 +0800

    loading spinner
    did up loading spinner files

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home';
 import PetMgm from './pages/PetMgm';
@@ -9,27 +9,56 @@ import TaskChecklist from './pages/TaskChecklist';
 import ExpenseTracker from './pages/ExpenseTracker';
 import Journal from './pages/Journal';
 import SocialPage from './pages/SocialPage';
+import Spinner from './components/Spinner';
+import { LoadingProvider, useLoading } from './components/LoadingContext';
+
+function RouteChangeHandler() {
+  const location = useLocation();
+  const {setLoading} = useLoading();
+
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => setLoading(false), 400); // simulate page load delay
+    return () => clearTimeout(timeout);
+  }, [location.pathname]);
+
+  return null;
+}
+
+function AppRoutes() {
+  const {loading} = useLoading();
+
+  return (
+    <>
+      {loading && <Spinner />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/task-checklist" element={<TaskChecklist />} />
+        <Route path="/journal" element={<Journal />} />
+        <Route path="/social-page" element={<SocialPage />} />
+        <Route path="/expense-tracker" element={<ExpenseTracker />} />
+        <Route path="/petmgm" element={<PetMgm />} />
+      </Routes>
+    </>
+  );
+}
 
 function App() {
-   return (
-     <Router>
-       <div className="App">
-         <div className="content">
-           <Routes>
-             <Route path="/" element={<Login />} />
-             <Route path="/login" element={<Login />} />
-             <Route path="/home" element={<Home />} />
-             <Route path="/forgot-password" element={<ForgotPassword />} />
-             <Route path="/task-checklist" element={<TaskChecklist />} />
-             <Route path="/journal" element={<Journal />} />
-             <Route path="/social-page" element={<SocialPage />} />
-             <Route path="/expense-tracker" element={<ExpenseTracker />} />
-             <Route path="/petmgm" element={<PetMgm />} />
-            </Routes>
-         </div>
-       </div>
-     </Router>
-   );
+  return (
+    <LoadingProvider>
+      <Router>
+        <RouteChangeHandler />
+        <div className="App">
+          <div className="content">
+            <AppRoutes />
+          </div>
+        </div>
+      </Router>
+    </LoadingProvider>
+  );
 }
 
 export default App;
