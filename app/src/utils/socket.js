4d45987b1456fd1fc7d7804a0fae5baaa67084f2
seanseethesean <takapvppro@gmail.com>
2025-07-27 00:21:31 +0800

    test if actions exit

--- app/src/utils/socket.js
+++ app/src/utils/socket.js
@@ -3,6 +3,15 @@ import { io } from "socket.io-client";
 let socket;
 
 export function getSocket() {
+  if (process.env.NODE_ENV === "test") {
+    // Return a dummy socket object in tests
+    return {
+      on: () => {},
+      off: () => {},
+      emit: () => {},
+    };
+  }
+
   if (!socket) {
     socket = io("https://petpulse-backend.onrender.com", {
       transports: ["websocket", "polling"],
